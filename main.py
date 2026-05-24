035ea76d2147823fe937a15cdabfcc08ca105a67
Jorge Alejandro Torres Paez <joretopa7@gmail.com>
2026-05-24 13:50:05 -0500

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI #import uvicorn
 
 nombre_pro_clientes = FastAPI() #nombre_pro_clientes = FastAPI(title="Proyecto de clientes", description="Este es un proyecto de clientes a desarrollar", version="1.0.0")
-
+#PRIMER ENDPOINT MENSAJE
 @nombre_pro_clientes.get("/") #@nombre_pro_clientes.get("/mensaje") #ruta para mostrar el mensaje
 def mensaje():
     return{"mensaje": "Este es el proyecto de clientes a desarrollar"}#retorna el mensaje en formato JSON
-
+#SEGUNDO ENDPOINT LISTA CLIENTES
 @nombre_pro_clientes.get("/clientes")#ruta para mostrar la lista de clientes
 def clientes():
     clientes = ["Jorge Alejandro Torres Paez","Sara Jasmin Cruz Calderon","Santiago Buitrago Goyeneche","Jonny Guerrero","Eddy Santiago Caro"] 
